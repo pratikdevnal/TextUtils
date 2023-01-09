66f8d8b975a6f637b7f8750cbe50001e446ae246
Pratik <pratik.devnal@gmail.com>
2023-01-10 03:25:56 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import './App.css';
 function App() {
@@ -27,29 +26,6 @@ function App() {
   </div>
 </nav>
     </>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> 92873d58ff09528d73e9f8978a1c4f79bd02b24c
   );
 }
 
