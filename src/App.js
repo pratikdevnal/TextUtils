3f3b48324fe2a3774f06947df013da12d8c63d4f
Pratik <pratik.devnal@gmail.com>
2023-01-12 20:23:04 +0530

    About Dark mode added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Navbar from './components/Navbar'
 import './App.css';
-import TextForm from './components/TextForm';
-// import About from './components/About';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
     <>
 <Navbar title="TextUtils" aboutText="About"/>
 <div className="container my-3">
-<TextForm heading="Enter the Text To Analyze"/>
-{/* <About/> */}
+{/* <TextForm heading="Enter the Text To Analyze"/> */}
+<About/>
 
 </div>
 
